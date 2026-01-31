79918cff8c397237a6dff0a7cc915b55654ced9a
Igor Gerasimov <gerasimowigor@gmail.com>
2026-01-31 22:27:03 +0100

    github pipelines

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the cluster"
   type        = string
-  default     = "1.34"
+  default     = "1.32"
 }
 
 variable "vpc_id" {
